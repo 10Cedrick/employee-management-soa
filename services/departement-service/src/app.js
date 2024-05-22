afe57bf78f2938f8a8f4d30faa71bee02bd5c191
Cedrick Rakotoarivelo <cedrickrakotoarivelo@gmail.com>
2024-05-22 15:03:31 +0300

    Setting up department service ✨

---
+++ services/departement-service/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const http = require('http').Server(app);
+
+app.use(
+    bodyParser.urlencoded({
+      extended: true
+    })
+);
+app.use(cors({origin: '*'}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World from department service API! 🌈🌈' });
+});
+
+
+http.listen(process.env.PORT, () =>
+    console.log(`App listening on http://localhost:${process.env.PORT} ! 🚀`)
+);
